546ee70bb7267bc853bda960adb947c675613b14
emmanuel966 <emmanuelngniado@yahoo.fr>
2024-08-07 16:21:23 +0200

    index.php

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+// Afficher un message de bienvenue
+echo "<h1>Bonjour, monde !</h1>";
+
+// Afficher la date actuelle
+echo "<p>La date et l'heure actuelles sont : " . date('d-m-Y H:i:s') . "</p>";
+?>
